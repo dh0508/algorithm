c6c094fd65bd6dcfc0f75df345ec2e94b82d2464
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-03-31 01:50:33 +0900

    [Silver V] Title: 연속인가? ?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long k,a,b,c,d;
+    cin >> k>>a>>b>>c>>d;
+    if (a*k+b==c*k+d) {
+        cout << "Yes "<<a*k+b;
+    }
+    else {
+        cout << "No";
+    }
+
+    return 0;
+}
