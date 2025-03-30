4a5848dae5429ab348778a43164dc0277069e160
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-03-31 01:35:47 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int T; cin >> T;
+    int i = 1;
+    while (T--) {
+        int a, b; cin >> a >> b;
+        cout << "Case "<< i<<": "<<a+b<<endl;
+        i ++;
+    }
+
+    return 0;
+}
