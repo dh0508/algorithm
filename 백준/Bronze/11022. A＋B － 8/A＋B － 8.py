dc0335f4a043ac44b930035454e1cccc148ccc59
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-03-31 01:40:23 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,8 @@
+import sys
+def input():
+    return sys.stdin.readline()
+
+test_case = int(input())
+for i in range(test_case):
+    a, b = map(int,input().split())
+    print('Case #',i+1,': ',a,' + ',b,' = ', (a+b), sep='')
