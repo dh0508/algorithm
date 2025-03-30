5eeac6bd9e66639d8f4a1f426fd172831f0d6a71
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-03-31 01:38:53 +0900

    [Silver IV] Title: 스택, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+def input():
+    return sys.stdin.readline()
+
+def solution(cmd, num = 'x'):
+    if cmd == 'push':
+        l.append(num)
+    if cmd == 'pop':
+        try:
+            print(l.pop(-1))
+        except:
+            print(-1)
+    if cmd == 'size':
+        print(len(l))
+    if cmd == 'empty':
+        if len(l) == 0:
+            print(1)
+        else:
+            print(0)
+    if cmd == 'top':
+        try:
+            print(l[-1])
+        except:
+            print(-1)
+n = int(input())
+l = []
+for _ in range(n):
+    i = input().strip()
+    try:
+        cmd, num = i.split()
+        solution(cmd, num)
+    except:
+        solution(i)
