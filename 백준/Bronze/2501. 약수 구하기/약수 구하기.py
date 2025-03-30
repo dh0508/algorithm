08f5c7e98159c9e89a1406f9c8cc301459bbfe68
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-03-31 01:24:53 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+def input():
+    return sys.stdin.readline()
+
+N, K = map(int, input().split())
+count = 0
+for i in range(1, N+1):
+    if N%i == 0:
+        count += 1
+        if count == K:
+            print(i)
+            break
+if count < K:
+    print(0)
