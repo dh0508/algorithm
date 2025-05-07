8ba5a105822570a47bd49feb607626d4b275fa96
홍동형 <158579651+dh0508@users.noreply.github.com>
2025-05-07 18:42:07 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+
+int main() {
+    int N, M; cin >> N >> M;
+    vector<int> dp(N + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        dp[i] = dp[i - 1];
+        if (i >= M) {
+            dp[i] = (dp[i] + dp[i - M]) % MOD;
+        }
+    }
+    cout << dp[N];
+    return 0;
+}
